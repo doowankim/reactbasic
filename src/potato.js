68297cd9e8044f90bbd5fbd8744874088a483c39
Doowankim <rlaendhks11@gmail.com>
2019-11-02 16:09:28 +0900

    add rating propTypes

--- src/potato.js
+++ src/potato.js
@@ -1,12 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Potato({name, picture}) {
+function Potato({name, picture, rating}) {
     return (
         <div>
             <h2>I like {name}</h2>
             <img src={picture} />
+            <h4>{rating}/5.0</h4>
         </div>
     );
 }
+
+//name, picture(속성값) 규정
+Potato.propTypes = {
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    rating: PropTypes.number
+};
 
 export default Potato;
