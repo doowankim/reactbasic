1c327dedadace0803a81d9f4c6b946f275fc5db3
Doowankim <rlaendhks11@gmail.com>
2019-11-02 15:34:30 +0900

    first create component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Potato from "./potato";
 
 function App() {
-  return (
+  return ( //html (화면에 보여지는 부분)
+      //className="APP" : CSS
+      //div : html
     <div className="App">
-
+      <h1>abcdhjghgjhgjk</h1>
+      <h2>abdakjlfaskj</h2>
+      <Potato />
     </div>
   );
 }
