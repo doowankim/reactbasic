cd3bac96c6cea85dd8a00b3fb1ae4655e9b3eedf
Doowankim <rlaendhks11@gmail.com>
2019-11-02 15:45:20 +0900

    multiple component

--- src/potato.js
+++ src/potato.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Potato({ favorite }) {
-    return <h3>I love {favorite}</h3>;
+function Potato({name, picture}) {
+    return (
+        <div>
+            <h2>I like {name}</h2>
+            <img src={picture} />
+        </div>
+    );
 }
 
 export default Potato;
