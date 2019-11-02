b26a8aaa4b3d5bdc40165dc8036edc01e29167e2
Doowankim <rlaendhks11@gmail.com>
2019-11-02 16:43:21 +0900

    plainning the movie component

--- src/App.js
+++ src/App.js
@@ -2,41 +2,25 @@ import React, {Component} from 'react';
 
 
 class App extends Component {
-
-  // 함수, 상태 값, 변수 기타등등 선언
-
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-
-  componentDidMount() { //불러오고 나서의 행동
-    console.log("componentDidMount");
-  }
-
-  componentDidUpdate() { // 행동을 취했을 때
-    console.log("componentDidUpdate");
-  }
-
-  componentWillMount() { //불러오기 전의 행동
-    console.log("componentWillMount");
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    }, 6000) //timeout: 1/1000초
   }
 
 
   render() {
     //상수, 변수 선언하는 곳
+    const {isLoading} = this.state;
     return (
         //html
+        //isLoading이 참이면 왼쪽 출력, 거짓이면 오른쪽 출력
         <div>
-          {/*{}: javascript를 불러옴. this: App.js를 뜻함 */}
-          <h1>The number is: {this.state.count}</h1>
-          <button onClick={this.add}>add</button>
-          <button onClick={this.minus}>minus</button>
+          {isLoading ? "Loading..." : "We are ready"}
         </div>
     );
   }
